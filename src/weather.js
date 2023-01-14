37b2dc3d54af8be52b35d3a3cc5f7fcc7f5ea773
ThanapawnTony <113745605+ThanapawnTony@users.noreply.github.com>
2023-01-14 11:06:49 +0700

    add eventListener

--- src/weather.js
+++ src/weather.js
@@ -10,12 +10,26 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerHTML = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/hr";
-    }
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
+
+document.querySelector(".search button").addEventListener("click", function() {
+        weather.search();
+    });
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+        if(event.key == "Enter") {
+            weather.search();
+        }
+    })
+
+weather.fetchWeather("Vientiane");
